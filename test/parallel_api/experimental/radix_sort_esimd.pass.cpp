ec4b3a21d28e7d13d4304a7a6fee8a8b9af79442
Sergey Kopienko <sergey.kopienko@intel.com>
2023-03-14 09:49:08 +0100

    test/parallel_api/experimental/radix_sort_esimd.pass.cpp - add file header

--- test/parallel_api/experimental/radix_sort_esimd.pass.cpp
+++ test/parallel_api/experimental/radix_sort_esimd.pass.cpp
@@ -1,3 +1,18 @@
+// -*- C++ -*-
+//===-- rasix_sort_esimd.pass.cpp -----------------------------------------===//
+//
+// Copyright (C) Intel Corporation
+//
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+// This file incorporates work covered by the following copyright and permission
+// notice:
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+//
+//===----------------------------------------------------------------------===//
+
 #define USE_ESIMD_SORT 1
 #include <oneapi/dpl/execution>
 #include <oneapi/dpl/algorithm>
